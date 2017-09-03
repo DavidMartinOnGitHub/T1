939fa61d6a9986ddb5353d975537da97d74a7f6d
David Martin <DavidMartinOnGitHub.gmail.com>
2017-09-03 15:46:08 -0700

    initial check-in

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FTD2XX_NET;
+
+namespace Direct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RequestMicroNIRFirmware();
+
+            Console.WriteLine("DONE");
+
+            // Console.WriteLine("Press any key to continue...");
+            // Console.ReadKey();
+        }
+
+        //---------------------------
+
+        private static bool RequestMicroNIRFirmware()
+        {
+            string serialNumber = "N1-00152";
+
+            Console.WriteLine("Serial Number = {0}", serialNumber);
+
+            FTD2XX_NET.FTDI ftdi = new FTD2XX_NET.FTDI();
+
+            Console.WriteLine(">>OpenBySerialNumber");
+
+            FTDI.FT_STATUS ftStatus = ftdi.OpenBySerialNumber(serialNumber);
+            if (ftStatus != FTDI.FT_STATUS.FT_OK)
+            {
+                string msg = string.Format("OpenBySerialNumber failed with '{0}' on Serial Number '{1}'", ftStatus.ToString(), serialNumber);
+                throw new Exception(msg);
+            }
+            Console.WriteLine("<<OpenBySerialNumber");
+
+            ftdi.ResetDevice();
+            ftdi.SetTimeouts(200, 200);
+
+
+            string command = "V\r"; // firmware
+            int dwell = 50;
+
+            Console.WriteLine(">>Purge");
+            ftdi.Purge(FTD2XX_NET.FTDI.FT_PURGE.FT_PURGE_RX);
+            Console.WriteLine("<<Purge");
+
+            Console.WriteLine(">>Write");
+            uint numBytesWritten = 255;
+            byte[] cmd = System.Text.Encoding.ASCII.GetBytes(command);
+            ftStatus = ftdi.Write(cmd, cmd.Length, ref numBytesWritten);
+            Console.WriteLine("<<Write");
+
+            System.Threading.Thread.Sleep(dwell);
+
+            Console.WriteLine(">>Read");
+            uint numBytesRead = 0;
+            byte[] rxbuff = new byte[512];
+            ftStatus = ftdi.Read(rxbuff, 512, ref numBytesRead);
+            Console.WriteLine("<<Read");
+
+            string response = ConvertResponseBytesToString(rxbuff);
+            Console.WriteLine("RESPONSE = '{0}'", response);
+
+            if (ftdi != null)
+            {
+                ftdi.Close();
+                ftdi = null;
+            }
+
+            return true;
+        }
+
+        private static string ConvertResponseBytesToString(byte[] bytes)
+        {
+            int idx = -1;
+            for (idx = 0; idx < bytes.Length; idx++)
+            {
+                byte ch = bytes[idx];
+
+                if (ch == 13 || ch == 0 || ch == 10) break; // break on CR, NULL or LF
+            }
+
+            string response = System.Text.Encoding.ASCII.GetString(bytes, 0, idx);
+            return response;
+        }
+        //---------------------------
+
+
+    }
+}
